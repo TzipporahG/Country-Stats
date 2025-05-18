cd30671ec1c4966b9f7130ad2d8c8430a21fce4a
Tzipporah G <tgordon@wits.edu>
2025-05-18 17:49:14 -0400

    fixed infoBox component to allow for category name

--- country-stats-app/src/InfoBox.jsx
+++ country-stats-app/src/InfoBox.jsx
@@ -1,11 +1,13 @@
 import { InfoBoxHeader } from "./InfoBoxHeader";
 import { BulletList } from "./BulletList";
 
-export function InfoBox({ header, items }) {
+export function InfoBox({ header, categories, items }) {
     return (
         <div className="info-box">
             <InfoBoxHeader header={header} />
-            <BulletList items={items} />
+            {categories.map((category, index) => (
+                <BulletList key={index} category={category} items={items[index]} />
+            ))}
         </div>
     );
 }
