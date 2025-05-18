6d90fe599c191608ab0ea63d723476817dcf38f4
Sara Nechama Isenberg <snisenberg@wits.edu>
2025-05-18 17:52:34 -0400

    Adding a search page without much on it

--- country-stats-app/src/App.jsx
+++ country-stats-app/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { InfoPage } from './Pages/InfoPage'
+import { SearchPage } from './Pages/SearchPage'
 function App() {
   return (
 
     <Routes>
-      {/* <Route path="Country-Stats/" element={<SearchPage/>}/> */}
-      <Route path="Country-Stats/info" element={<InfoPage/>}/>
+      <Route path="Country-Stats/" element={<SearchPage/>}/>
+      <Route path="/info" element={<InfoPage/>}/>
     </Routes>
     
   )
---
+++ country-stats-app/src/Pages/SearchPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export function SearchPage() {
+    return (
+        <>
+            <h1>Search Page components go here</h1>
+            <Link to="/info">Go to Info Page</Link>
+        </>
+    );
+}
