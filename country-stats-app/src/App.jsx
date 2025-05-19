d703414ef9b75ec58b19f3b8285c2f6a96341f78
Tzipporah G <tgordon@wits.edu>
2025-05-19 14:35:48 -0400

    created an about us page

--- country-stats-app/src/App.jsx
+++ country-stats-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { InfoPage } from './Pages/InfoPage'
+import { AboutUs } from './Pages/AboutUs'
 function App() {
   return (
 
     <Routes>
       {/* <Route path="Country-Stats/" element={<SearchPage/>}/> */}
       <Route path="Country-Stats/info" element={<InfoPage/>}/>
+      <Route path="Country-Stats/about-us" element={<AboutUs/>}/>
     </Routes>
     
   )
